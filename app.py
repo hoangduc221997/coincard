af9c5094b2861f1c7cbd763d507d423ef64509ee
Nguyễn Ngân <nganjjang@Nguyens-MacBook-Air.local>
2018-06-04 20:46:31 +0700

    backend register

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import *
+from mongoengine import *
+import mlab
+from models.user import User
+
 app = Flask(__name__)
+
+mlab.connect()
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        form = request.form
+        username = form['username']
+        password = form['password']
+        fullname = form['fullname']
+        yob = form['yob']
+        email = form['email']
+        gender = form['gender']
+
+        user = User(username=username,
+                    password=password,
+                    fullname=fullname,
+                    yob=yob,
+                    email=email,
+                    gender=gender)
+        user.save()
+
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
   app.run(debug=True)
