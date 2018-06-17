348eee7221faa076d5c67818f8f534cf669a88b2
nganjjang <nganjjang@Nguyens-MacBook-Air.local>
2018-06-17 13:50:42 +0700

    ngan push

--- crawldata.py
+++ crawldata.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import math
 
 def get_coin_data():
     url="https://coinmarketcap.com/"
@@ -27,15 +28,23 @@ def get_coin_data():
                 name = td_list[td].find('a','currency-name-container').string
             elif td == 2:
                 marketcap = float(td_list[td].string)
+                marketcap = math.ceil(marketcap)
+                marketcap_str = "{:,}".format(marketcap)
             elif td == 3:
                 price = td_list[td].a.string
                 price = float(price.replace('$',''))
+                price = math.ceil(price)
+                price_str = "{:,}".format(price)
             elif td == 4:
                 volume = td_list[td].a.string
                 volume = float(volume.replace('$','').replace(',',''))
+                volume = math.ceil(volume)
+                volume_str = "{:,}".format(volume)
             elif td == 5:
                 supply = td_list[td].a.span.string
                 supply = float(supply.replace('$','').replace(',',''))
+                supply = math.ceil(supply)
+                supply_str = "{:,}".format(supply)
             elif td == 6:
                 change = td_list[td].string
                 change = float(change.replace('%','')) #unit of change is percentage (%)
@@ -43,9 +52,13 @@ def get_coin_data():
             "name": name,
             'image': image,
             "marketcap": marketcap,
+            "marketcap_str": marketcap_str,
             "price": price,
+            "price_str":price_str,
             "volume": volume,
+            "volume_str":volume_str,
             "supply": supply,
+            "supply_str":supply_str,
             "change": change
         }
         coin_data.append(coin_dict)
